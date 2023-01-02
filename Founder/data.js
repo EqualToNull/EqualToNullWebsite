1dd2f696291de15019e5ea440e773fb5ff92537b
EqualToNull <121730487+EqualToNull@users.noreply.github.com>
2023-01-02 13:04:00 +0800

    Add files via upload

---
+++ Founder/data.js
@@ -0,0 +1,20 @@
+const Post = {
+    1: {
+        DateAndTime: "2023 Jan 1 | 00:59",
+        Caption: "...",
+        Image: "Images/2023-Jan-01.png"
+    },
+
+    2: {
+        DateAndTime: "2023 Jan 1 | 11:58",
+        Caption: "...",
+        Image: "Drawings/2023-01-01_1158.jpg"
+    },
+
+    3: {
+        DateAndTime: "2023 Jan 2 | 09:17",
+        Caption: "The start of today sucked...",
+        Image: "Drawings/2023-01-02_0858.jpg"
+    }
+}
+
